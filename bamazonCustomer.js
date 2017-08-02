cc2628a4ecce654987a4192bd3be62276fd87e79
Amanda Wittenstein <mashka.lewis@gmail.com>
2017-08-01 20:01:54 -0500

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,33 +1,37 @@
 
+//print to console how many units available, price, department, name, pulled from SQL database
+//ask how many they want of each item, return error if not enough. Use cli-table to display
+//in table format. Use inquierer to print to screen. Give total price. Update SQL database to reflect
+//items purchased.
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8889,
   user: "root", //Your username
-  password: "root", //Your password
-  database: "Bamazon"
+  password: " ", //Your password
+  database: "bamazon"
 });
 
 function startBuy()
 {
 connection.query("SELECT * FROM Products", function(error, response)
-{
+  {
   if (error) throw error;
-   console.log(Welcome to Bamazon! Enjoy your shopping.);
-
-});
-
-  var table = new Table
-  ({
-    head:["ItemID", "ProductName", "DepartmentName", "Price", "StockQuantity"]
-  });
+   console.log("Welcome to Bamazon! Enjoy your shopping.");
+   console.log("What would you like to purchase?")
+   var table =
+   ({
+     head:["ItemID", "ProductName", "DepartmentName", "Price", "StockQuantity"]
+   });
+ });
+startBuy();
 
 
 function runSearch()
   {
-  inquirer.prompt;
+  inquirer.prompt([
     {
       name: "product_ID",
       type: "checkbox",
@@ -36,9 +40,9 @@ function runSearch()
       [
         "ItemID"
       ]
-    }
+    }])
     .then(function(answer)
-     {if (answer.product_ID === "Continue Shopping?")}
-
-}
+      {if (answer.product_ID === "Continue Shopping?"){}
+})
+   }
 }
